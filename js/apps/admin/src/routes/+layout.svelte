db3494daceeb29f880021595885dae984ed0b17b
didiermun <didiermunezer38@gmail.com>
2023-04-18 00:42:38 +0200

    feat: static labellisation

--- js/apps/admin/src/routes/+layout.svelte
+++ js/apps/admin/src/routes/+layout.svelte
@@ -5,7 +5,10 @@
 
 <main class="bg-silver min-h-screen flex gap-0">
 	<Sidebar/>
-	<div class="flex-1">
-	<slot />
+	<div class="flex-1 px-8">
+		<div class="w-full flex justify-end">
+			<img class="py-6 pr-3 pb-4" src="favicon.png" alt="">
+		</div>
+		<slot />
 	</div>
 </main>
--- js/apps/admin/tailwind.config.cjs
+++ js/apps/admin/tailwind.config.cjs
@@ -11,16 +11,28 @@ module.exports = {
 			colors: {
 				silver:{
 					200: '#131313',
+					hr: "#A7A7A7",
 					DEFAULT: '#F8F8F8'
 				},
 				blue:{
 					DEFAULT: '#7CC0CE',
 					vert:'#007D82',
-					2: '#00979C'
+					2: '#00979C',
+					less: "#82C3D0",
+					gradient: ""
 				},
 				'dark-green':{
 					DEFAULT: '#2A4A51'
+				},
+				orange:{
+					DEFAULT: '#F7931F'
+				},
+				primary:{
+					DEFAULT: 'rgba(247, 147, 31, 0.1)'
 				}
+			},
+			boxShadow:{
+				label:"0px 56px 37px -32px rgba(42, 74, 81, 0.18)",
 			}
 		}
 	},
