17947e5d5263180224fe0fa568b3caf7782ce721
didiermun <didiermunezer38@gmail.com>
2023-04-18 21:34:19 +0200

    fix: netlify to vercel, unwanted packages

--- js/apps/admin/svelte.config.js
+++ js/apps/admin/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,29 +10,10 @@ const config = {
 			postcss: true
 		})
 	],
-
-	// Docs: https://github.com/sveltejs/kit/blob/master/packages/adapter-netlify/README.md
 	kit: {
 		adapter: adapter({
-			split: false,
-
-			// nov 2022
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions. however, also uses esbuild, which as of nov 2022 has a bug on netlify
-			// https://github.com/sveltejs/kit/issues/7839#issuecomment-1328605300
-
-			// dec 2022 - moved back to true since we're using esbuild again
-			edge: true,
+			
 		}),
-		// https://kit.svelte.dev/docs/configuration#csp
-		// csp: {
-		// 	directives: {
-		// 		'script-src': ['self']
-		// 	},
-		// 	reportOnly: {
-		// 		'script-src': ['self']
-		// 	}
-		// }
 	}
 };
 
